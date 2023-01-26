ace198c598a08a7c9d7544d72d1ca733c2a9411c
tomatija <tomazic.matija@gmail.com>
2023-01-26 20:24:35 +0100

    renamed models in menzaDatabase, added urls.py to menzaDatabase

--- menzarBackend/menzarBackend/urls.py
+++ menzarBackend/menzarBackend/urls.py
@@ -5,4 +5,5 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('menzaDatabase.urls'))
 ]
--- menzarBackend/users/models.py
+++ menzarBackend/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class user(models.Model):
+class Users(models.Model):
     login_name = models.CharField(max_length=50, default="example_name")
     display_name = models.CharField(max_length=50, default="example_name")
     password = models.CharField(max_length=50, default="password")

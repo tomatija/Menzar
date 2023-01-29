464fe249aa230102761f58828739b7544c35946e
zvaktualno <tadej.zivic@hotmail.com>
2023-01-29 14:51:05 +0100

    fix

--- backend/build.sh
+++ backend/build.sh
@@ -7,3 +7,5 @@ poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('nevemcejetupametnaideja', 'admin@myproject.com', 'butwedoinitanyway')" | python manage.py shell

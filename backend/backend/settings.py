73b3cb2d0f6abe6d4aaf6a61c96e7901ed47bde1
zvaktualno <tadej.zivic@hotmail.com>
2023-01-29 15:10:56 +0100

    added a test cronjob

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,11 +34,15 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+CRONJOBS = [
+    ('*/1 * * * *', 'menzaDatabase.cron.my_scheduled_job')
+]
 
 # Application definition
 
 INSTALLED_APPS = [
     'menzaDatabase.apps.MenzadatabaseConfig',
+    'django_crontab',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- backend/build.sh
+++ backend/build.sh
@@ -9,3 +9,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('nevemcejetupametnaideja', 'admin@myproject.com', 'butwedoinitanyway')" | python manage.py shell
+python manage.py crontab add

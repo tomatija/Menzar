c71ca93a049bee2207062318bea66e570c1232fe
zvaktualno <tadej.zivic@hotmail.com>
2023-01-29 15:14:38 +0100

    fix

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ spletna stran za ogled menz
 
 Powershell: Set-ExecutionPolicy RemoteSigned
 According to [THIS GUIDE](https://www.sharepointdiary.com/2014/03/fix-for-powershell-script-cannot-be-loaded-because-running-scripts-is-disabled-on-this-system.html)
+
+
+# Cron job
+
+Cron jobs won't function on windows machines, so they work only on deployed version of the app.
--- backend/build.sh
+++ backend/build.sh
@@ -8,5 +8,6 @@ poetry install
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('nevemcejetupametnaideja', 'admin@myproject.com', 'butwedoinitanyway')" | python manage.py shell
+#Uncomment this if there is no superuser
+#echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('nevemcejetupametnaideja', 'admin@myproject.com', 'butwedoinitanyway')" | python manage.py shell
 python manage.py crontab add

d2cd6e60414fca43e668f57c91c5c36de4209c4b
zvaktualno <tadej.zivic@hotmail.com>
2023-03-13 22:34:45 +0100

    visual changes

---
+++ frontend/src/components/Diner/FavoriteFab.js
@@ -0,0 +1,32 @@
+import { Fab, Icon } from "@mui/material";
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import './Diner.css';
+  
+const FavoriteFab = ({ isFavorite, display, onClick }) => {
+    console.log(isFavorite);
+    const favoriteButton = display ? (
+        <Icon
+
+            className={isFavorite ? 'favorite-icon filled' : 'favorite-icon empty'}
+            component={isFavorite ? FavoriteIcon : FavoriteBorderIcon}
+            onClick={onClick}
+        />
+    ) : null;
+    
+    return (
+        <Fab
+            className="favorite-fab"
+            style={{
+                position: "absolute",
+                top: 10,
+                right: 10
+            }}
+            aria-label="like"
+        >
+            {favoriteButton}
+        </Fab>
+    );
+};
+
+export default FavoriteFab;

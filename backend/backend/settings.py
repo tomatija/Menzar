ed0067f9d096f20a38a4444c366454d8909b95b6
zvaktualno <tadej.zivic@hotmail.com>
2023-03-20 18:16:38 +0100

    hosting improvements

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,7 +53,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5!o3+^w*(+4zb_v_qk0fkg_q&$)b+c93b@lcdq+l&zz-1tmh@&'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'django-insecure-5!o3+^w*(+4zb_v_qk0fkg_q&$)b+c93b@lcdq+l&zz-1tmh@&',)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
@@ -64,9 +65,10 @@ BACKEND_URL = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 FRONTEND_URL = os.environ.get('FRONTEND_URL')
 
 # TODO: REMOVE THIS '*' ALLOWED HOSTS
-ALLOWED_HOSTS = [
-    '*'
-]
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
@@ -158,7 +160,7 @@ if DEBUG == True:
 else:
     DATABASES["default"] = dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='',
         conn_max_age=600
     )
 
